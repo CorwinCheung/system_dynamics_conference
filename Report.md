0b49ee840e50a543140855b105b2c5c74d40a123
Corwin <gmail@corwincheung.com>
2023-08-28 21:09:41 -0400

    texty text done

---
+++ Report.md
@@ -0,0 +1,236 @@
+# Text Analysis
+
+## Techniques used:
+
+- Textblob library sentiment analysis to classify responses into positive, negative, and neutral
+- TF-IDF Vectorization to extract keywords and n-grams from the text
+- Latent Dirichlet Allocation(LDA) topic modeling, to get representative responses for each topics
+- Named Entity Recognition(NER) with spaCy library to find important entities
+- LDA also used to get top words for each topic and guess binding topic categories
+- WordClouds for graphic show of most common non trivial words
+
+## Results
+
+## Word clouds:
+
+Overall Wordcloud
+![image](figures/WordCloud_Overall.png)
+
+Wordcloud for In person event - 2019
+![image](figures/WordCloud_In_Person.png)
+
+Wordcloud for Virtual event - 2021
+![image](figures/WordCloud_Virtual.png)
+
+Wordvloud for Hybrid event - 2022
+![image](figures/WordCloud_Hybrid.png)
+
+# Data Sentiment Analysis
+
+## Overall Data Sentiment Analysis
+
+- Positive: 1517
+- Negative: 261
+- Neutral: 1125
+
+## Best Thing That Happened to You Data Sentiment Analysis
+
+First few responses:
+
+- "I could meet for the first time with people that I've met regularly but only online."
+- "Expanding my perspective with studies in many fields."
+- "I got to present at a parallel session which was my first experience presenting as well as doing so online at ISDC."
+- "Yaman's talk was inspiring to me. John Sterman's plenary."
+- "Seeing that my volunteering work helped make the hybrid conference successful."
+
+- Positive: 200
+- Negative: 8
+- Neutral: 139
+
+## Worst Thing That Happened to You Data Sentiment Analysis
+
+First few responses:
+
+- "Not having feedback or not having enough time to talk and learn from experts."
+- "Time zone conflict between Frankfurt and US Mountain."
+- "I was planning to join in person but could not and was quite upset about it and the FOMO of not being able to attend the banquet as well as socials with people present in person."
+- "None."
+- "Going to sleep during a session entirely due to time zone differences."
+
+- Positive: 93
+- Negative: 65
+- Neutral: 150
+
+# Keyword Analysis - Through sentiment analysis
+
+## One Word Keywords
+
+| Good Term  | Importance |
+| ---------- | ---------- |
+| sessions   | 0.029851   |
+| great      | 0.027946   |
+| good       | 0.027821   |
+| conference | 0.027390   |
+| people     | 0.026458   |
+
+| Bad Term   | Importance |
+| ---------- | ---------- |
+| difficult  | 0.033215   |
+| conference | 0.032399   |
+| expensive  | 0.031004   |
+| sessions   | 0.028219   |
+| time       | 0.027320   |
+
+## Two Word Keywords
+
+| Good Term         | Importance |
+| ----------------- | ---------- |
+| recorded sessions | 0.003788   |
+| new people        | 0.003722   |
+| parallel sessions | 0.003716   |
+| learning new      | 0.003332   |
+| great job         | 0.003112   |
+
+| Bad Term            | Importance |
+| ------------------- | ---------- |
+| difficult navigate  | 0.012097   |
+| time zone           | 0.007955   |
+| bad happened        | 0.006466   |
+| conference schedule | 0.005614   |
+| expensive location  | 0.005103   |
+
+## Three Word Keywords
+
+| Good Term                           | Importance |
+| ----------------------------------- | ---------- |
+| meeting new people                  | 0.002217   |
+| learning new things                 | 0.001403   |
+| format increases facilitates        | 0.001076   |
+| increases facilitates participation | 0.001076   |
+| conference believe virtual          | 0.001076   |
+
+| Bad Term                            | Importance |
+| ----------------------------------- | ---------- |
+| virtual participation socializing   | 0.004199   |
+| participation socializing difficult | 0.004199   |
+| missing sessions timing             | 0.003831   |
+| getting tired absorb                | 0.003831   |
+| portion lunches small               | 0.003831   |
+
+## LDA Topic modeling with 3 components over all responses:
+
+## Correlated Topics
+
+### Topic 1: Conference Venue, Catering, and Attendee Feedback
+
+- hotel
+- feedback
+- food
+- workshops
+- location
+- posters
+- lunch
+- airbnb
+- schedule
+- research
+
+### Topic 2: Accommodation, Business Amenities, and Travel Logistics
+
+- hotel
+- room
+- business
+- lunch
+- excellent
+- food
+- posters
+- use
+- travel
+- boston
+
+### Topic 3: Virtual Conference Experience and Accessibility
+
+- virtual
+- able
+- zoom
+- schedule
+- person
+- format
+- access
+- workshops
+- echo
+- feedback
+
+## Important terms by year discovered through TF-IDF
+
+| Year | Keywords                                                                      |
+| ---- | ----------------------------------------------------------------------------- |
+| 2022 | 'virtual', 'hybrid', 'person', 'technical', 'feedback', 'website', 'access'   |
+| 2021 | 'zoom', 'virtual', 'feedback', 'technical', 'workshops', 'schedule', 'dialog' |
+| 2020 | 'virtual', 'echo', 'zoom', 'recorded', 'able', 'chat', 'format'               |
+| 2019 | 'albuquerque', 'hotel', 'colloquium', 'workshops', 'lunch', 'none', 'airbnb'  |
+| 2018 | 'feedback', 'airbnb', 'hostel', 'iceland', 'location', 'wip', 'hotel'         |
+| 2016 | 'hotel', 'delft', 'food', 'posters', 'dinner', 'colleagues', 'location'       |
+| 2015 | 'hotel', 'hyatt', 'total', 'responses', 'cambridge', 'room', 'isolated'       |
+| 2014 | 'lunch', 'easy', 'venue', 'view', 'posters', 'delft', 'hotel'                 |
+| 2013 | 'hotel', 'room', 'mit', 'lunch', 'business', 'posters', 'excellent'           |
+
+## Important entites discovered through NER(Named Entity Recognition) with spaCy
+
+| Keyword   | Frequency |
+| --------- | --------- |
+| phd       | 33        |
+| summer    | 25        |
+| isdc      | 18        |
+| wip       | 17        |
+| boston    | 14        |
+| europe    | 13        |
+| cambridge | 12        |
+| inperson  | 9         |
+| frankfurt | 8         |
+| sunday    | 8         |
+
+## Necessary Changes
+
+The changes are necessary for the following reasons, as uncovered by the text analysis:
+
+The analysis showed that attendees valued networking but found it difficult to have one-to-one or small group interactions in the virtual platform. This indicates a need to improve virtual interaction features.
+
+Attendees expressed concerns about the venue's isolation and lack of nearby dining options, indicating a need for a more accessible and convenient location.
+
+The analysis revealed concerns about high registration and hotel fees, indicating a need to make the conference more affordable.
+
+Some attendees expressed disappointment with the quality and relevance of sessions and feedback, indicating a need for a more rigorous selection and review process.
+
+Specific concerns were raised about the summer school and banquet, indicating a need to gather more detailed feedback and address these issues.
+
+The text analysis did not directly address this, but enhancing the online experience is crucial in the current trend towards virtual and hybrid conferences.
+
+## Suggestions
+
+### Enhance Virtual Interaction
+
+- Add features for private and small group discussions in the virtual platform.
+
+### Select Accessible Venue
+
+- Choose a central location with public transport and dining options.
+
+### Reduce Costs
+
+- Offer tiered registration fees, early-bird discounts, and scholarships.
+
+### Improve Session Quality
+
+- Implement a rigorous review process for presentations and provide clear guidelines for feedback.
+
+### Address Event Concerns
+
+- Gather detailed feedback on the summer school and banquet to address specific issues.
+
+### Improve Online Experience
+
+- Incorporate interactive elements in virtual sessions and make recordings available post-conference.
+
+Implementing these recommendations will improve the attendee experience and the success of future conferences.
+
+Understand that these suggestions come with a caveat as the vast majority of attendees responses were very posistive and there could be adverse selection in which attendants filled out the survey.
--- detailed_analysis.py
+++ detailed_analysis.py
@@ -9,7 +9,7 @@
 
 # For text data, we must do this: Sentiment Analysis: Use a sentiment analysis model to
 # determine the sentiment of each response. There are several pre-trained models available
-# , like the one in the TextBlob library or the VADER model in the nltk library.
+# , like the TextBlob library.
 
 # Identify Positive and Negative Responses: Based on the sentiment scores, classify the
 # responses into positive, negative, and neutral categories.
@@ -22,9 +22,199 @@
 # more of, while the most frequent keywords in the negative responses will give an
 # indication of the things that attendees disliked and want to change.
 
+from textblob import TextBlob
+import pandas as pd
+import numpy as np
+import re
+import ast
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
+def basic_preprocess_text(text):
+    # Remove special characters, numbers, and punctuations
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    # Convert text to lowercase
+    text = text.lower()
+    return text
+
+
+def split_into_individual_responses(text):
+    responses = [response for sublist in text.values.flatten()
+                 for response in sublist]
+
+    individual_responses = [response.split(
+        '\n') for response in responses if response]
+
+    individual_responses = [
+        item for sublist in individual_responses for item in sublist if item]
+
+    return individual_responses
+
+
+def analyze_sentiments(data):
+    # Flatten the dataframe to get a list of all responses
+    individual_responses = split_into_individual_responses(data)
+
+    # Display the first few responses
+    print("first few responses: ")
+    print(individual_responses[:5])
+
+    # Perform sentiment analysis on the responses
+    sentiments = [
+        TextBlob(response).sentiment.polarity for response in individual_responses]
+
+    # Classify the responses into positive, negative, and neutral
+    positive_responses = [individual_responses[i]
+                          for i in range(len(individual_responses)) if sentiments[i] > 0.1]
+    negative_responses = [individual_responses[i]
+                          for i in range(len(individual_responses)) if sentiments[i] < -0.1]
+    neutral_responses = [individual_responses[i] for i in range(
+        len(individual_responses)) if sentiments[i] >= -0.1 and sentiments[i] <= 0.1]
+
+    return positive_responses, negative_responses, neutral_responses
+
+# Display the topics
+
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i]
+              for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+
+def get_top_n_representative_responses_for_each_topic(responses, n):
+    # Only consider the first 50 words of each response
+    responses = [' '.join(response.split()[:50]) for response in responses]
+
+    # Tokenize the text and convert it to bag-of-words representation
+    vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
+    tf = vectorizer.fit_transform(responses)
+    lda = LatentDirichletAllocation(
+        n_components=3, max_iter=5, learning_method='online', learning_offset=50., random_state=0).fit(tf)
+
+    # Get topic distribution for each response
+    topic_distributions = lda.transform(tf)
+
+    # Get the top N most representative responses for each topic
+    most_representative_responses = {}
+    for topic_idx in range(topic_distributions.shape[1]):
+        top_n_representative_response_idxs = topic_distributions[:, topic_idx].argsort(
+        )[-n:]
+        most_representative_responses[topic_idx] = [
+            responses[i] for i in top_n_representative_response_idxs]
+
+    return most_representative_responses
+
+
+def extract_keywords(texts, num_keywords=5, ngram=1):
+    # Create a TF-IDF Vectorizer object
+    vectorizer = TfidfVectorizer(ngram_range=(
+        ngram, ngram), stop_words='english')
+    # Fit the texts to the vectorizer
+    tfidf_matrix = vectorizer.fit_transform(texts)
+    # Get the words from the vocabulary
+    feature_names = vectorizer.get_feature_names_out()
+    # Get the importance of each word
+    importance = np.asarray(tfidf_matrix.mean(axis=0)).ravel().tolist()
+    # Create a dataframe with the words and their importance
+    df = pd.DataFrame({'Term': feature_names, 'Importance': importance})
+    # Sort the dataframe by importance
+    df = df.sort_values('Importance', ascending=False)
+    return df.head(num_keywords)
+
 
 def main():
     print("starting!!")
+
+    # Part 1: dimensionality reduction and text clustering to generate insights on
+    # the survey responses
+
+    # Part 2: sentiment analysis to see the tones per answer
+
+    # Load the data
+    file_path = 'text_responses.csv'
+    data = pd.read_csv(file_path)
+
+    print("Text Data:" + str(data.head()))
+
+    # # Apply the basic preprocessing to each column of the dataset
+    basic_preprocessed_data = data.applymap(
+        lambda x: basic_preprocess_text(str(x)) if pd.notna(x) else '')
+
+    basic_preprocessed_data = basic_preprocessed_data.applymap(
+        lambda x: ast.literal_eval(str(x)) if '[' in str(x) else [str(x)])
+
+    positive_responses, negative_responses, neutral_responses = analyze_sentiments(
+        basic_preprocessed_data)
+
+    print("Overall data sentiment analysis: ")
+    print("positive: " + str(len(positive_responses)), "negative: " + str(len(
+        negative_responses)), "neutral: " + str(len(neutral_responses)))
+
+    # To do a sanity check, we will look at the sentiment analysis of the responses
+    # from the worst and best things that happened to you at the conference
+
+    print("Best thing that happened to you data sentiment analysis: ")
+
+    best_thing = basic_preprocessed_data["What was the best thing that happened to you at the conference?"]
+    best_pos_resp, best_neg_resp, best_neu_resp = analyze_sentiments(
+        best_thing)
+
+    print("positive: " + str(len(best_pos_resp)), "negative: " + str(len(
+        best_neg_resp)), "neutral: " + str(len(best_neu_resp)))
+
+    print("worst thing that happened to you data sentiment analysis: ")
+
+    worst_thing = basic_preprocessed_data["What was the worst thing that happened to you at the conference?"]
+    worst_pos_resp, worst_neg_resp, worst_neu_resp = analyze_sentiments(
+        worst_thing)
+
+    print("positive: " + str(len(worst_pos_resp)), "negative: " + str(len(
+        worst_neg_resp)), "neutral: " + str(len(worst_neu_resp)))
+
+    # Extract keywords with Tf-IDF vectorizer to see what was most mentioned in the positive
+    # and negative responses
+    # Make a Top 5 list of things liked and disliked.
+    positive_keywords = extract_keywords(positive_responses)
+    negative_keywords = extract_keywords(negative_responses)
+
+    print("\nOne words: ")
+    print(positive_keywords)
+    print(negative_keywords)
+
+    positive_bigrams = extract_keywords(positive_responses, ngram=2)
+    negative_bigrams = extract_keywords(negative_responses, ngram=2)
+
+    print("\nTwo words: ")
+    print(positive_bigrams)
+    print(negative_bigrams)
+
+    positive_trigrams = extract_keywords(positive_responses, ngram=3)
+    negative_trigrams = extract_keywords(negative_responses, ngram=3)
+
+    print("\nThree words: ")
+    print(positive_trigrams)
+    print(negative_trigrams)
+
+    # Let's look for phrases instead of just individual terms
+
+    # Extract the most representative answers from the positive and negative responses
+    # Get two responses for the three most important categories
+
+    positive_most_representative_responses = get_top_n_representative_responses_for_each_topic(
+        positive_responses, 2)
+    negative_most_representative_responses = get_top_n_representative_responses_for_each_topic(
+        negative_responses, 2)
+
+    print("\n\n\n Most representative repsonses: \n \n ")
+
+    print("Positive most representative responses")
+    print(positive_most_representative_responses)
+
+    print("Negative most representative responses")
+    print(negative_most_representative_responses)
 
 
 main()
